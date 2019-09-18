511cf96331e521a998594be0d8c2a47f4314f96e
heechangkang <heechang1997@gmail.com>
2019-09-18 16:26:21 +0900

    calculator-website project

--- 2-3_calculator-website/calculator.js
+++ 2-3_calculator-website/calculator.js
@@ -1,12 +1,29 @@
 //jshint esversion:6
+// bodyparser
 const express = require("express");
+const bodyParser = require("body-parser");
+
 const app = express();
+app.use(bodyParser.urlencoded({extended: true}));
+
 
 app.get("/", function(req, res){
     res.sendFile(__dirname + "/index.html");
     //console.log(__dirname); --> tells the current folder
 });
 
-app.listen(3001, function(){
-    console.log(" simple cal server started on 3001");
-})
+app.post("/", function(req, res){
+
+    // console.log(req.body.num1); // be able to get data from html
+
+    var num1 = Number(req.body.num1);
+    var num2 = Number(req.body.num2);
+    var result = num1 + num2;
+
+    res.send("result : "+ result);
+
+});
+
+app.listen(3000, function(){
+    console.log("simple cal server started on 3000");
+});
