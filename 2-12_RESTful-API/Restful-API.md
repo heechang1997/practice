3cbf27f923285c3f8782f214ce9ed54e7412a0c8
heechangkang <heechang1997@gmail.com>
2019-12-02 23:59:21 +0900

    RESTful API

---
+++ 2-12_RESTful-API/Restful-API.md
@@ -0,0 +1,19 @@
+# RESTful API
+
+REpresentational State Transfer
+
+client -HTTP,FTP --> Server
+
+type of architecture : Restful, GraphQL, Falcor
+
+## Restful API architecture
+
+use http request verbs : GET(read), POST(create), PUT(update), PATCH(update specific ting), DELETE
+
+Restful Routing
+
+## Creating RESTful API : codeWIKI API
+
+- npm : body parser, mongoose, ejs, express
+- app.js, server code
+- MongoDB : db:wikiDB, articles->title, content
---
+++ 2-12_RESTful-API/app.js
@@ -0,0 +1,47 @@
+/*
+- npm : body parser, mongoose, ejs, express
+- app.js, server code
+- MongoDB : db:wikiDB, articles->title, content
+*/
+const express = require("express");
+const bodyParser = require("body-parser");
+const ejs = require("ejs");
+const mongoose = require('mongoose');
+
+// Server Configuration
+const app = express();
+
+app.set('view engine', 'ejs');
+
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+app.use(express.static("public"));
+
+
+// setting up DataBase
+mongoose.connect("mongodb://localhost:27017/wikiDB", { useNewUrlParser: true });
+
+const articleSchema = {
+    title: String,
+    content: String
+}
+const Article = mongoose.model("Article", articleSchema)
+
+
+//
+app.get("/articles", function(req, res) {
+    Article.find(function(err, articles) {
+        if (!err){
+            res.send(articles);
+        }else {
+            res.send(err);
+        }
+
+    })
+})
+
+// port of the server
+app.listen(3000, function () {
+    console.log("Server started on port 3000");
+});
